f19f98f638db041acb2ca8c162faae1df51ce255
gnehs <jayuiop@gmail.com>
2019-03-17 11:48:50 +0800

    新增 Source 翻譯

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   router-link.card(v-if="to",:to="to",:data-source="source||undefined")
-      .image(:style="{backgroundImage:`url(\"${pokaBg}\")`}",v-if="pokaBg")
+      .image(v-if="pokaBg",:style="{backgroundImage:`url(\"${pokaBg}\")`}")
         md-tooltip {{pokaTitle}}
       .image(v-else)
         md-icon(class="icon-white", :class="pokaIcon")
@@ -8,7 +8,7 @@
       .title {{pokaTitle}}
       .subtitle {{pokaSubtitle}}
   a.card(v-else,:data-source="source||undefined")
-      .image(:style="{backgroundImage:`url(\"${pokaBg}\")`}",v-if="pokaBg")
+      .image(v-if="pokaBg",:style="{backgroundImage:`url(\"${pokaBg}\")`}")
         md-tooltip {{pokaTitle}}
       .image(v-else)
         md-icon(class="icon-white", :class="pokaIcon")
@@ -21,12 +21,20 @@
 export default {
   name: "poka-card",
   props: {
-    pokaBg: { default: false },
+    pokaBg: { default: null },
     source: { default: "" },
     pokaTitle: String,
     pokaSubtitle: String,
     to: String,
     pokaIcon: { default: "outline-play_circle_filled", type: String }
+  },
+  created() {
+    // source
+    if (this.source == "Netease2") {
+      this.source = i18n.t("moduleShowName_Netease");
+    } else if (this.source == "DSM") {
+      this.source = i18n.t("moduleShowName_DSM");
+    }
   }
 };
 </script>
