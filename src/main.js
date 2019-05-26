b92f0aec4c27742f0b0f6d30c8d73c3937c44dd1
gnehs <jayuiop@gmail.com>
2019-05-26 19:36:04 +0800

    修正前端無法使用

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@/assets/poka.js'
 import '@/assets/MaterialSlider.css'
 import '@/assets/icon_outline.css'
 
