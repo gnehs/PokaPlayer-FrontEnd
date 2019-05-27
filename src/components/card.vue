106af47e68e155908015e1ad257e446b3d08c951
gnehs <jayuiop@gmail.com>
2019-05-27 19:27:34 +0800

    修正 icon card 裡面沒有 icon
    可是太陽餅裡面還是沒太陽：（

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,48 +1,48 @@
 <template lang="pug">
-  router-link.card(v-if="to",:to="to",:data-source="parsed_source||undefined")
-      .image(v-if="parsed_pokaBg",:style="{backgroundImage:`url(\"${parsed_pokaBg}\")`}")
-        md-tooltip {{pokaTitle}}
-      .image(v-else)
-        md-icon(class="icon-white", :class="pokaIcon")
-        md-tooltip {{pokaTitle}}
-      .title {{pokaTitle}}
-      .subtitle {{pokaSubtitle}}
-  a.card(v-else,:data-source="parsed_source||undefined")
-      .image(v-if="parsed_pokaBg",:style="{backgroundImage:`url(\"${parsed_pokaBg}\")`}")
-        md-tooltip {{pokaTitle}}
-      .image(v-else)
-        md-icon(class="icon-white", :class="pokaIcon")
-        md-tooltip {{pokaTitle}}
-      .title {{pokaTitle}}
-      .subtitle {{pokaSubtitle}}
+	router-link.card(v-if="to",:to="to",:data-source="parsed_source||undefined")
+		.image(v-if="parsed_pokaBg",:style="{backgroundImage:`url(\"${parsed_pokaBg}\")`}")
+			md-tooltip {{pokaTitle}}
+		.image(v-else)
+			md-icon(class="icon-white", :class="pokaIcon")
+			md-tooltip {{pokaTitle}}
+		.title {{pokaTitle}}
+		.subtitle {{pokaSubtitle}}
+	a.card(v-else,:data-source="parsed_source||undefined")
+		.image(v-if="parsed_pokaBg",:style="{backgroundImage:`url(\"${parsed_pokaBg}\")`}")
+			md-tooltip {{pokaTitle}}
+		.image(v-else)
+			md-icon(class="icon-white", :class="pokaIcon")
+			md-tooltip {{pokaTitle}}
+		.title {{pokaTitle}}
+		.subtitle {{pokaSubtitle}}
 </template>
 
 <script>
 export default {
-  name: "poka-card",
-  props: {
-    pokaBg: { default: null },
-    source: { default: "" },
-    pokaTitle: String,
-    pokaSubtitle: String,
-    to: String,
-    pokaIcon: { default: "outline-play_circle_filled", type: String }
-  },
-  data: () => ({ parsed_source: null ,parsed_pokaBg:null}),
-  created() {
-    // source
-    if (this.source == "Netease2") {
-      this.parsed_source = i18n.t("moduleShowName_Netease");
-    } else if (this.source == "DSM") {
-      this.parsed_source = i18n.t("moduleShowName_DSM");
-    } else {
-      this.parsed_source = this.source;
-    }
-    if(typeof this.pokaBg==="string"&&this.pokaBg.startsWith("http")){
-      this.parsed_pokaBg=this.pokaBg
-    }else{
-      this.parsed_pokaBg=_setting('server')+this.pokaBg
-    }
-  }
+	name: "poka-card",
+	props: {
+		pokaBg: { default: null },
+		source: { default: "" },
+		pokaTitle: String,
+		pokaSubtitle: String,
+		to: String,
+		pokaIcon: { default: "outline-play_circle_filled", type: String }
+	},
+	data: () => ({ parsed_source: null, parsed_pokaBg: null }),
+	created() {
+		// source
+		if (this.source == "Netease2") {
+			this.parsed_source = i18n.t("moduleShowName_Netease");
+		} else if (this.source == "DSM") {
+			this.parsed_source = i18n.t("moduleShowName_DSM");
+		} else {
+			this.parsed_source = this.source;
+		}
+		if (typeof this.pokaBg === "string" && this.pokaBg.startsWith("http")) {
+			this.parsed_pokaBg = this.pokaBg;
+		} else if (this.pokaBg) {
+			this.parsed_pokaBg = _setting("server") + this.pokaBg;
+		}
+	}
 };
 </script>
