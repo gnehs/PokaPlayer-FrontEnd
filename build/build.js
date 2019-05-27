ea2d62ec150af89dca3181f23dc18a0e18774821
gnehs <jayuiop@gmail.com>
2019-05-27 20:41:04 +0800

    build 完成自己把檔案給 poka

--- build/build.js
+++ build/build.js
@@ -3,6 +3,7 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
+const fs = require('fs')
 const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
@@ -15,27 +16,40 @@ const spinner = ora('building for production...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, (err, stats) => {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+	if (err) throw err
+	webpack(webpackConfig, (err, stats) => {
+		spinner.stop()
+		if (err) throw err
+		process.stdout.write(stats.toString({
+			colors: true,
+			modules: false,
+			children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+			chunks: false,
+			chunkModules: false
+		}) + '\n\n')
 
-    if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
-      process.exit(1)
-    }
+		if (stats.hasErrors()) {
+			console.log(chalk.red('  Build failed with errors.\n'))
+			process.exit(1)
+		}
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
+		console.log(chalk.cyan('  Build complete.\n'))
+		console.log(chalk.yellow(
+			'  Tip: built files are meant to be served over an HTTP server.\n' +
+			'  Opening index.html over file:// won\'t work.\n'
+		))
+		if (fs.existsSync('copy.sh')) {
+			console.log(chalk.cyan('  複製檔案到 PokaPlayer public...\n'))
+			const exec = require('child_process').exec;
+			var copyToPoka = exec('sh copy.sh', (error, stdout, stderr) => {
+				console.log(stdout);
+				console.log(stderr);
+				if (error !== null) {
+					console.log(`exec error: ${error}`);
+				}
+				console.log(chalk.cyan('  複製完成\n'))
+			});
+		}
+
+	})
 })
