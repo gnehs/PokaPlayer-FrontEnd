38a4b92a1e99aefa6e90f86f342b81b2307e338f
gnehs <jayuiop@gmail.com>
2019-05-28 18:31:53 +0800

    Home 新增 loader

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,29 +1,25 @@
 <template>
-  <div>
-    <poka-header
-      :title="$t('header_welcome')"
-      :subtitle="$t('header_version',{version:poka_version})"
-    />
-    <poka-parse-multiple :data="data"/>
-  </div>
+	<div>
+		<poka-header
+			:title="$t('header_welcome')"
+			:subtitle="$t('header_version',{version:poka_version})"
+		/>
+		<poka-loader v-if="!data"/>
+		<poka-parse-multiple v-else :data="data"/>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  created() {
-    this.axios.get(_setting(`server`) + "/pokaapi/home/").then(response => {
-      this.data = response.data;
-    });
-    this.axios.get(_setting(`server`) + "/info/").then(response => {
-      this.poka_version = response.data.version;
-    });
-  },
-  data() {
-    return {
-      data: null,
-      poka_version: null
-    };
-  }
+	name: "Home",
+	created() {
+		this.axios.get(_setting(`server`) + "/pokaapi/home/").then(response => {
+			this.data = response.data;
+		});
+		this.axios.get(_setting(`server`) + "/info/").then(response => {
+			this.poka_version = response.data.version;
+		});
+	},
+	data: () => ({ data: null, poka_version: null })
 };
 </script>
