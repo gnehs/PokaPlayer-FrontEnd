cf4b263f97d97d733ab276550d6be8e0505b35ae
gnehs <jayuiop@gmail.com>
2019-06-03 17:18:12 +0800

    loader 加個 margin top

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-    <md-progress-spinner md-mode="indeterminate" :md-stroke="3" :md-diameter="30"></md-progress-spinner>
-  </div>
+	<div>
+		<md-progress-spinner md-mode="indeterminate" :md-stroke="3" :md-diameter="30"></md-progress-spinner>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "poka-loader"
+	name: "poka-loader"
 };
 </script>
 <style lang="sass" scoped>
 div
-    text-align: center
+	text-align: center
+	margin-top: 8px
 </style>
