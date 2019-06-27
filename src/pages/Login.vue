6621ea7c232a5c0b6362bbd483a70da1cba18d0e
gnehs <jayuiop@gmail.com>
2019-06-27 17:08:37 +0800

    重新設計登入頁面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,44 +1,79 @@
 <template>
-	<div>
-		<form class="md-layout" v-on:submit.prevent="login">
-			<md-card class="md-layout-item md-size-50 md-small-size-100">
-				<md-card-media-cover>
-					<md-card-media md-ratio="16:9" class="card-banner">
-						<img src="https://source.unsplash.com/random">
-					</md-card-media>
+	<form class="md-layout" v-on:submit.prevent="login">
+		<md-card class="md-layout-item">
+			<md-card-media-cover>
+				<md-card-media md-ratio="16:9" class="card-banner">
+					<img class="header-img" src="https://source.unsplash.com/daily">
+				</md-card-media>
 
-					<md-card-area>
-						<md-card-header>
-							<span class="md-title">Login</span>
-						</md-card-header>
-					</md-card-area>
-				</md-card-media-cover>
+				<md-card-area>
+					<md-card-header>
+						<span class="md-title">PokaPlayer</span>
+					</md-card-header>
+				</md-card-area>
+			</md-card-media-cover>
 
-				<md-card-content>
-					<md-field>
-						<md-icon class="md-accent" v-if="serverError">warning</md-icon>
-						<md-icon class="md-accent" v-else>link</md-icon>
-						<label for="server">Server</label>
-						<md-input type="text" name="server" v-model.trim="server" :disabled="logining"/>
-					</md-field>
-					<md-field>
-						<md-icon class="md-accent" v-if="passwordError">warning</md-icon>
-						<md-icon class="md-accent" v-else>vpn_key</md-icon>
-						<label for="password">Password</label>
-						<md-input type="password" name="password" v-model="password" :disabled="logining"/>
-					</md-field>
-				</md-card-content>
+			<md-card-content>
+				<md-field>
+					<md-icon class="md-accent" v-if="serverError">warning</md-icon>
+					<md-icon class="md-accent" v-else>link</md-icon>
+					<label for="server">Server</label>
+					<md-input type="text" name="server" v-model.trim="server" :disabled="logining"/>
+				</md-field>
+				<md-field>
+					<md-icon class="md-accent" v-if="passwordError">warning</md-icon>
+					<md-icon class="md-accent" v-else>vpn_key</md-icon>
+					<label for="password">Password</label>
+					<md-input type="password" name="password" v-model="password" :disabled="logining"/>
+				</md-field>
+			</md-card-content>
 
-				<md-progress-bar md-mode="indeterminate" v-if="logining"/>
+			<md-progress-bar md-mode="indeterminate" v-if="logining"/>
 
-				<md-card-actions>
-					<md-button type="submit" class="md-raised md-primary" :disabled="logining">{{$t('login')}}</md-button>
-				</md-card-actions>
-			</md-card>
-		</form>
-	</div>
+			<md-card-actions>
+				<md-button type="submit" class="md-raised md-primary" :disabled="logining">{{$t('login')}}</md-button>
+			</md-card-actions>
+		</md-card>
+	</form>
 </template>
 
+<style lang="scss" scoped>
+	.md-card-media-cover {
+		background: #505050;
+	}
+	.card-banner::before {
+		padding-top: 170px !important;
+	}
+	.md-progress-bar {
+		position: absolute;
+		top: 0;
+		right: 0;
+		left: 0;
+	}
+	.header-img {
+		transform: translateY(-50%) scale(1.2);
+		filter: blur(3px) brightness(70%);
+	}
+	form {
+		height: 90%;
+		justify-content: center;
+		align-items: center;
+		.md-card {
+			margin: 0 auto;
+			max-width: 400px;
+			border-radius: 8px;
+			overflow: hidden;
+			box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2);
+			.md-button {
+				background: linear-gradient(
+					120deg,
+					#4251fb 0%,
+					#0401a3 100%
+				) !important;
+			}
+		}
+	}
+</style>
 <script>
 export default {
 	name: "Login",
@@ -104,15 +139,3 @@ export default {
 	}
 };
 </script>
-
-<style lang="scss" scoped>
-	.md-progress-bar {
-		position: absolute;
-		top: 0;
-		right: 0;
-		left: 0;
-	}
-	form .md-card {
-		margin: 0 auto;
-	}
-</style>
