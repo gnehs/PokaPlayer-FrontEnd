52845e16ed4fcfbb5ecf20e301629fc4c33141a8
gnehs <jayuiop@gmail.com>
2019-11-23 12:45:26 +0800

    fixed #16

---
+++ src/layout/empty.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<md-app>
+			<md-app-content>
+				<router-view />
+			</md-app-content>
+		</md-app>
+	</div>
+</template>
+<script>
+export default {
+	name: "App"
+};
+</script>
+<style lang="sass" scoped>
+.md-app-content
+	overflow-y: auto
+	overflow-x: hidden
+.md-app
+	min-height: calc(var(--vh, 1vh) * 100)
+	max-height: calc(var(--vh, 1vh) * 100)
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import defaultLayout from '@/layout/default'
+import emptyLayout from '@/layout/empty'
+
 import Album from '@/pages/Album'
 import AlbumSongs from '@/pages/AlbumSongs'
 import Artist from '@/pages/Artist'
@@ -27,138 +30,152 @@ import SettingUser from '@/pages/SettingUser'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [{
-    path: '/lyric',
-    name: 'Lyric',
-    component: Lyric
-  }, {
-    path: '/album/songs/:source/:id',
-    name: 'AlbumSongs',
-    component: AlbumSongs,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/album',
-    name: 'Album',
-    component: Album
-  }, {
-    path: '/artist/:source/:id',
-    name: 'ArtistAlbum',
-    component: ArtistAlbum,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/artist',
-    name: 'Artist',
-    component: Artist
-  }, {
-    path: '/composer/:source/:id',
-    name: 'ComposerAlbum',
-    component: ComposerAlbum,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/composer',
-    name: 'Composer',
-    component: Composer
-  }, {
-    path: '/folder/:source/:id',
-    name: 'folderFiles',
-    component: Folder,
-    meta: {
-      transitionName: 'slide',
-      root: false
-    }
-  }, {
-    path: '/folder',
-    name: 'Folder',
-    component: Folder,
-    meta: {
-      root: true
-    }
-  }, {
-    path: '/install',
-    name: 'Install',
-    component: Install
-  }, {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }, {
-    path: '/now',
-    name: 'NowPlaying',
-    component: NowPlaying
-  }, {
-    path: '/playlist/:source/:id',
-    component: PlaylistSongs,
-    name: 'PlaylistSongs',
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/playlist',
-    name: 'Playlist',
-    component: Playlist
-  }, {
-    path: '/search',
-    name: 'Search',
-    component: Search
-  }, {
-    path: '/setting',
-    name: 'Setting',
-    component: Setting
-  }, {
-    path: '/setting/network',
-    name: 'SettingNetwork',
-    component: SettingNetwork,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/setting/customize',
-    name: 'SettingCustomize',
-    component: SettingCustomize,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/setting/lang',
-    name: 'SettingLang',
-    component: SettingLang,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/setting/system',
-    name: 'SettingSystem',
-    component: SettingSystem,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/setting/user',
-    name: 'SettingUser',
-    component: SettingUser,
-    meta: {
-      transitionName: 'slide'
-    }
-  }, {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }, {
-    path: '/notfound',
-    name: 'Notfound',
-    component: Notfound
-  }, {
-    path: '/',
-    redirect: '/home'
-  }, {
-    path: '*',
-    redirect: '/notfound'
-  }]
+	mode: 'history',
+	routes: [{
+			path: '/',
+			name: 'root',
+			component: defaultLayout,
+			redirect: '/home',
+			children: [{
+				path: '/lyric',
+				name: 'Lyric',
+				component: Lyric
+			}, {
+				path: '/album/songs/:source/:id',
+				name: 'AlbumSongs',
+				component: AlbumSongs,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/album',
+				name: 'Album',
+				component: Album
+			}, {
+				path: '/artist/:source/:id',
+				name: 'ArtistAlbum',
+				component: ArtistAlbum,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/artist',
+				name: 'Artist',
+				component: Artist
+			}, {
+				path: '/composer/:source/:id',
+				name: 'ComposerAlbum',
+				component: ComposerAlbum,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/composer',
+				name: 'Composer',
+				component: Composer
+			}, {
+				path: '/folder/:source/:id',
+				name: 'folderFiles',
+				component: Folder,
+				meta: {
+					transitionName: 'slide',
+					root: false
+				}
+			}, {
+				path: '/folder',
+				name: 'Folder',
+				component: Folder,
+				meta: {
+					root: true
+				}
+			}, {
+				path: '/now',
+				name: 'NowPlaying',
+				component: NowPlaying
+			}, {
+				path: '/playlist/:source/:id',
+				component: PlaylistSongs,
+				name: 'PlaylistSongs',
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/playlist',
+				name: 'Playlist',
+				component: Playlist
+			}, {
+				path: '/search',
+				name: 'Search',
+				component: Search
+			}, {
+				path: '/setting',
+				name: 'Setting',
+				component: Setting
+			}, {
+				path: '/setting/network',
+				name: 'SettingNetwork',
+				component: SettingNetwork,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/setting/customize',
+				name: 'SettingCustomize',
+				component: SettingCustomize,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/setting/lang',
+				name: 'SettingLang',
+				component: SettingLang,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/setting/system',
+				name: 'SettingSystem',
+				component: SettingSystem,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/setting/user',
+				name: 'SettingUser',
+				component: SettingUser,
+				meta: {
+					transitionName: 'slide'
+				}
+			}, {
+				path: '/home',
+				name: 'Home',
+				component: Home
+			}, {
+				path: '/notfound',
+				name: 'Notfound',
+				component: Notfound
+			}, {
+				path: '',
+				redirect: '/home'
+			}, ]
+		}, {
+			path: '/',
+			name: 'empty',
+			component: emptyLayout,
+			redirect: '/dashboard',
+			children: [{
+				path: '/install',
+				name: 'Install',
+				component: Install
+			}, {
+				path: '/login',
+				name: 'Login',
+				component: Login
+			}, ]
+		},
+		{
+			path: '*',
+			redirect: '/notfound'
+		}
+	]
 })
