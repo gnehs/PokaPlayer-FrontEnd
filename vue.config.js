27ad47288f3cefdec47bbbba2115ae18d7a28d87
gnehs <jayuiop@gmail.com>
2020-01-19 21:56:51 +0800

    修正 theme-color

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const vueConfig = {
   "transpileDependencies": [
     "vuetify"
   ]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  vueConfig.chainWebpack = (config) => {
+    config.plugin('html').init((Plugin, args) => {
+      const newArgs = {
+        ...args[0],
+      };
+      newArgs.minify.removeAttributeQuotes = false;
+      return new Plugin(newArgs);
+    });
+  };
+}
+
+module.exports = vueConfig;
