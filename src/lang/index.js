fd27c0669354ab9f46f9f47f639bdefa371f3492
gnehs <jayuiop@gmail.com>
2020-07-23 15:00:41 +0800

    fixed: lang hot reloading

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,35 +1,30 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-const zh_CN = require('./zh-CN.js').default
-const zh_TW = require('./zh-TW.js').default
-const en_US = require('./en-US.js').default
+let langList = {
+    zh_CN: require('./zh-CN.js').default,
+    zh_TW: require('./zh-TW.js').default,
+    en_US: require('./en-US.js').default
+}
 
 Vue.use(VueI18n)
 const locale = window._setting('lang')
-const messages = {
-    zh_CN: zh_CN,
-    zh_TW: zh_TW,
-    en_US: en_US
-}
+const messages = langList
 
 const i18n = new VueI18n({
     /** 默認值 */
     locale,
     fallbackLocale: 'en_US',
     messages,
-    missing: (locale, key, vm) => {
-        console.log(locale, key, vm)
-        return key
-    },
+    missing: (locale, key, vm) => key,
 })
 window.i18n = i18n
 // Hot updates
 if (module.hot) {
     module.hot.accept(['./zh-CN.js', './zh-TW.js', './en-US.js'], function () {
-        i18n.setLocaleMessage('zh_CN', require('./zh-CN.js'))
-        i18n.setLocaleMessage('en_US', require('./en-US.js'))
-        i18n.setLocaleMessage('zh_TW', require('./zh-TW.js'))
+        i18n.setLocaleMessage('zh_CN', require('./zh-CN.js').default)
+        i18n.setLocaleMessage('en_US', require('./en-US.js').default)
+        i18n.setLocaleMessage('zh_TW', require('./zh-TW.js').default)
     })
 }
 export default i18n
