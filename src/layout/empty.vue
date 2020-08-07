a5d50ea634af15952523460ad2c29376a368fc5f
gnehs <jayuiop@gmail.com>
2020-08-07 19:34:31 +0800

    移除 snackbar 殘留程式碼

--- src/layout/empty.vue
+++ src/layout/empty.vue
@@ -7,9 +7,6 @@
 import Vue from "vue";
 export default {
 	name: "App",
-	data: () => ({
-		snackbar: { show: false, message: ``, timeout: null },
-	}),
 	created() {
 		// 狀態欄顏色
 		document.getElementsByTagName('meta')["theme-color"].content = this.$vuetify.theme.isDark ? "rgb(47, 43, 62)" : "rgb(245, 245, 245)"
