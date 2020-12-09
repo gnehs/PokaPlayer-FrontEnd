53e49887935aa2d5eab8bb2f7d312bcae4e2635d
gnehs <jayuiop@gmail.com>
2020-12-09 16:25:00 +0800

    新增於演出者與作曲者頁面切換版面按鈕

--- src/components/parse-artists.vue
+++ src/components/parse-artists.vue
@@ -1,37 +1,55 @@
 <template>
 	<div>
-		<poka-cards v-if="artistView=='card'">
-			<poka-card
-				v-for="{name, cover, id, source} in data"
-				:key="id"
-				:to="`/artist/${encodeURIComponent(source)}/${encodeURIComponent(id||'unknown')}`"
-				:poka-bg="cover||false"
-				:poka-title="name||'未知'"
-				:poka-subtitle="$t(`source.${source}`)"
-			/>
-		</poka-cards>
-		<div class="poka list" v-if="artistView=='list'">
-			<div
-				class="item"
-				style="user-select: none;"
-				v-for="{name, cover, id, source} in data"
-				:key="id"
-				@click="$router.push(`/artist/${encodeURIComponent(source)}/${encodeURIComponent(id||'unknown')}`)"
-				v-ripple
-			>
-				<div class="content">
-					<v-avatar size="28px" item>
-						<v-img :src="coverPaser(cover)" alt="cover" />
-					</v-avatar>
-					<div class="header">
-						<div class="head t-ellipsis">
-							{{name||'未知'}}
-							<span style="font-size: .7em;opacity: .7;font-weight: normal;">{{source}}</span>
+		<v-fade-transition>
+			<poka-cards v-show="artistView=='card'">
+				<poka-card
+					v-for="{name, cover, id, source} in data"
+					:key="id"
+					:to="`/artist/${encodeURIComponent(source)}/${encodeURIComponent(id||'unknown')}`"
+					:poka-bg="cover||false"
+					:poka-title="name||'未知'"
+					:poka-subtitle="$t(`source.${source}`)"
+				/>
+			</poka-cards>
+		</v-fade-transition>
+		<v-fade-transition>
+			<div class="poka two list" v-show="artistView=='list'">
+				<div
+					class="item"
+					style="user-select: none;"
+					v-for="{name, cover, id, source} in data"
+					:key="id"
+					@click="$router.push(`/artist/${encodeURIComponent(source)}/${encodeURIComponent(id||'unknown')}`)"
+					v-ripple
+				>
+					<div class="content">
+						<v-avatar size="28px" item>
+							<v-img :src="coverPaser(cover)" alt="cover" />
+						</v-avatar>
+						<div class="header">
+							<div class="head t-ellipsis">
+								{{name||'未知'}}
+								<span style="font-size: .7em;opacity: .7;font-weight: normal;">{{source}}</span>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</v-fade-transition>
+		<v-btn
+			color="primary"
+			fab
+			large
+			dark
+			bottom
+			right
+			fixed
+			style="bottom: calc(16px + 69px);"
+			@click="changeView"
+		>
+			<v-icon v-if="artistView=='card'" class="material-icons-outlined">list</v-icon>
+			<v-icon v-else class="material-icons-outlined">grid_on</v-icon>
+		</v-btn>
 	</div>
 </template>
 
@@ -51,6 +69,10 @@ export default {
 				return _setting(`server`) + cover
 			else
 				return _setting(`headerBgSource`)
+		},
+		changeView() {
+			this.artistView = this.artistView == 'list' ? 'card' : 'list'
+			_setting(`artistView`, this.artistView)
 		}
 	}
 };
