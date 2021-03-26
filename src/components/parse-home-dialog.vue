c9c43f5d741b4a2c24d12b6a1506d74c545dc430
gnehs <jayuiop@gmail.com>
2021-03-26 19:42:39 +0800

    fixed home dialog

--- src/components/parse-home-dialog.vue
+++ src/components/parse-home-dialog.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-dialog v-model="dialog" fullscreen scrollable transition="dialog-bottom-transition">
+	<v-bottom-sheet v-model="dialog" inset>
 		<template v-slot:activator="{ on }">
 			<poka-card
 				:poka-bg="false"
@@ -10,47 +10,59 @@
 				@click.native="dialog=true"
 			/>
 		</template>
-		<v-card class="mx-auto">
-			<v-toolbar dark color="primary" flat>
-				<v-toolbar-title>{{$t(data.title)}}</v-toolbar-title>
-				<v-chip class="ma-2" color="white" outlined pill>{{$t(`source.${data.source}`)}}</v-chip>
-				<v-spacer></v-spacer>
+		<v-card class="poka-home-card" v-if="dialog">
+			<div class="hw-header" style="line-height: normal;">
+				<div class="hw-title">{{$t(data.title)}}</div>
+				<div class="hw-subtitle">{{$t(`source.${data.source}`)}}</div>
+			</div>
+			<div class="chip-nav" v-if="filteredKeys.length>1">
+				<div
+					class="chip-nav-item"
+					v-for="itemName of filteredKeys"
+					:key="itemName"
+					:class="{active:active==itemName}"
+					@click="active=itemName"
+					v-ripple
+				>
+					<span>{{$t(itemName.substring(0, itemName.length-1))}}</span>
+				</div>
+			</div>
 
-				<v-btn icon dark @click="dialog = false">
-					<v-icon>mdi-close</v-icon>
-				</v-btn>
-
-				<template v-slot:extension>
-					<v-tabs v-model="tab" background-color="primary" color="white" slider-color="yellow">
-						<v-tab
-							v-for="itemName of Object.keys(data).filter(x=>x!='title'&&x!='source'&&x!='icon'&&data[x].length)"
-							:key="itemName"
-						>{{$t(itemName.substring(0, itemName.length-1))}}</v-tab>
-					</v-tabs>
-				</template>
-			</v-toolbar>
-			<v-card-text style="height: calc(100% - 64px)">
-				<v-tabs-items v-model="tab">
-					<v-tab-item
-						v-for="itemName of Object.keys(data).filter(x=>x!='title'&&x!='source'&&x!='icon'&&data[x].length) "
-						:key="itemName"
-					>
-						<poka-parse-songs v-if="itemName=='songs'" :data="data[itemName]" />
-						<poka-parse-albums v-if="itemName=='albums'" :data="data[itemName]" />
-						<poka-parse-playlists v-if="itemName=='playlists'" :data="data[itemName]" />
-						<poka-parse-folders v-if="itemName=='folders'" :data="data[itemName]" />
-						<poka-parse-composers v-if="itemName=='composers'" :data="data[itemName]" />
-						<poka-parse-artists v-if="itemName=='artists'" :data="data[itemName]" />
-					</v-tab-item>
-				</v-tabs-items>
-			</v-card-text>
+			<div v-for="itemName of filteredKeys" :key="itemName">
+				<div v-show="active==itemName">
+					<poka-parse-songs v-if="itemName=='songs'" :data="data[itemName]" />
+					<poka-parse-albums v-if="itemName=='albums'" :data="data[itemName]" />
+					<poka-parse-playlists v-if="itemName=='playlists' " :data="data[itemName]" />
+					<poka-parse-folders v-if="itemName=='folders' " :data="data[itemName]" />
+					<poka-parse-composers v-if="itemName=='composers' " :data="data[itemName]" />
+					<poka-parse-artists v-if="itemName=='artists' " :data="data[itemName]" />
+				</div>
+			</div>
 		</v-card>
-	</v-dialog>
+	</v-bottom-sheet>
 </template>
+<style lang="sass" scoped>
+.poka-home-card
+	padding: 16px
+	overflow: hidden auto
+	max-height: 80vh
+	position: relative
+</style>
 <script>
 export default {
 	name: "poka-parse-home-dialog",
 	props: ["data"],
-	data: () => ({ dialog: false, tab: null })
+	data() {
+		return ({
+			dialog: false,
+			tab: null,
+			active: null,
+			filteredKeys: []
+		})
+	},
+	created() {
+		this.filteredKeys = Object.keys(this.data).filter(x => x != 'title' && x != 'source' && x != 'icon' && this.data[x].length)
+		this.active = this.filteredKeys[0]
+	}
 };
 </script>
