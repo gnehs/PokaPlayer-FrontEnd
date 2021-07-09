baa1ccad5748480c2ce95ea7ea624c1ebb7bde7b
gnehs <jayuiop@gmail.com>
2021-07-10 02:09:56 +0800

    format code

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
-	<v-app class="page-container">
-		<transition name="fade" mode="out-in">
-			<router-view />
-		</transition>
-		<v-snackbar
-			v-model="snackbar.show"
-			:class="{'withBottomPlayer':snackbar.withBottomPlayer}"
-		>{{snackbar.message}}</v-snackbar>
-	</v-app>
+  <v-app class="page-container">
+    <router-view />
+    <v-snackbar
+      v-model="snackbar.show"
+      :class="{'withBottomPlayer':snackbar.withBottomPlayer}"
+    >{{snackbar.message}}</v-snackbar>
+  </v-app>
 </template>
 <script>
 import Vue from "vue";
 export default {
-	created() {
-		Vue.prototype.$deepCopy = window._deepcopy
-		Vue.prototype.$randomPlay = window._randomPlay;
-		Vue.prototype.$addSongs = window._addSongs;
-		//註冊點心條組件
-		Vue.prototype.$snackbar = (msg = ``, duration = 1500) => {
-			this.snackbar.message = msg;
-			this.snackbar.withBottomPlayer = document.querySelectorAll('.bottom-player').length
-			this.snackbar.show = true;
-			clearTimeout(this.snackbar.timeout);
-			this.snackbar.timeout = setTimeout(() => (this.snackbar.show = false), duration);
-		};
-		// set theme color
-		this.$vuetify.theme.themes.dark.primary = window._setting('theme')
-		this.$vuetify.theme.themes.light.primary = window._setting('theme')
-	},
-	data: () => ({
-		snackbar: { show: false, message: ``, timeout: null, withBottomPlayer: true },
-	})
-}
+  created() {
+    Vue.prototype.$deepCopy = window._deepcopy;
+    Vue.prototype.$randomPlay = window._randomPlay;
+    Vue.prototype.$addSongs = window._addSongs;
+    //註冊點心條組件
+    Vue.prototype.$snackbar = (msg = ``, duration = 1500) => {
+      this.snackbar.message = msg;
+      this.snackbar.withBottomPlayer = document.querySelectorAll(
+        ".bottom-player"
+      ).length;
+      this.snackbar.show = true;
+      clearTimeout(this.snackbar.timeout);
+      this.snackbar.timeout = setTimeout(
+        () => (this.snackbar.show = false),
+        duration
+      );
+    };
+    // set theme color
+    this.$vuetify.theme.themes.dark.primary = window._setting("theme");
+    this.$vuetify.theme.themes.light.primary = window._setting("theme");
+  },
+  data: () => ({
+    snackbar: {
+      show: false,
+      message: ``,
+      timeout: null,
+      withBottomPlayer: true
+    }
+  })
+};
 </script>
 <style lang="sass">
 @import "@/assets/pokaList.sass"
