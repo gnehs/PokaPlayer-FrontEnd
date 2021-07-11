1106672490a1b99dc1b3b0cf399e140e986bbefe
gnehs <jayuiop@gmail.com>
2021-07-11 23:23:10 +0800

    Optimize the album page loading screen

--- src/pages/Albums.vue
+++ src/pages/Albums.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    <poka-header :title="name || $t('album')" :subtitle="name && type != 'album' ? $t(type) : null" :blurbg="type != 'album'" :bg="cover" />
-    <library-menu />
+    <portal to="app-bar" v-if="type != 'album'">
+      <v-app-bar
+        color="#FFF"
+        clipped-left
+        app
+        :style="`box-shadow: 0px 0px 0px 1px ${$vuetify.theme.isDark ? 'rgba(255, 255, 255, 0.12)' : `rgb(0 0 0 / 20%)`}`"
+      >
+        <back icon />
+        <v-toolbar-title>{{ name }}</v-toolbar-title>
+        <v-spacer />
+        <poka-searchbar />
+      </v-app-bar>
+    </portal>
+    <poka-header v-if="type == 'album'" :title="name" :bg="cover" />
+    <poka-header v-else :title="$t(type)" blurbg :bg="cover" />
+    <library-menu v-if="type == 'album'" />
     <v-slide-y-reverse-transition>
       <poka-parse-albums v-if="data" :data="data.albums" />
     </v-slide-y-reverse-transition>
@@ -33,25 +47,27 @@ export default {
       this.data = null
       this.cover = _setting(`headerBgSource`)
 
-      let url,
-        type = this.$route.meta.type
+      let url
+      let type = this.$route.meta.type
       let { source, id } = this.$route.params
       if (id == 'unknown') id = ''
 
       if (type == 'album') {
+        this.name = this.$t('album')
         url = _setting(`server`) + '/pokaapi/albums/'
       } else {
-        this.name = 'Loading...'
+        this.name = ''
         url = `${this.server}/pokaapi/${type}Albums/?moduleName=${encodeURIComponent(source)}&id=${encodeURIComponent(id)}`
       }
       this.data = (await this.axios.get(url)).data
 
-      this.name = ''
       //get name & cover
-      this.axios.get(`/pokaapi/${type}/?moduleName=${encodeURIComponent(source)}&id=${encodeURIComponent(id)}`).then(response => {
-        this.cover = _setting(`server`) + response.data.cover
-        this.name = response.data.name
-      })
+      if (type != 'album') {
+        this.axios.get(`/pokaapi/${type}/?moduleName=${encodeURIComponent(source)}&id=${encodeURIComponent(id)}`).then(response => {
+          this.cover = _setting(`server`) + response.data.cover
+          this.name = response.data.name
+        })
+      }
     }
   }
 }
