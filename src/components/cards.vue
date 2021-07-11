220dbb0c3d0b40beb8c88035d9f961e8c89fccad
gnehs <jayuiop@gmail.com>
2021-07-11 16:29:10 +0800

    auto scale card size

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -13,188 +13,124 @@ export default {
 .theme--dark
 	.poka.cards
 		--text-color: #FFF
-.poka.cards
-	--text-color: #000
-	display: flex
-	flex-wrap: wrap
-	max-width: calc(100% + 8px * 2)
-	width: calc(100% + 8px * 2)
-	margin: -8px
-	.card
-		--size: calc(16.6% - 8px)
-		height: 0
-		width: var(--size)
-		font-size: 11px
-		margin: .8em 4px
-		background-position: center
-		background-size: cover
-		padding-bottom: var(--size)
-		padding: .475em .5em
-		display: block
-		height: auto
-		padding-bottom: 0
-		text-decoration: none
-		position: relative
-
-.poka.cards .card[data-source]:before
-	content: attr(data-source)
-	position: absolute
-	color: #FFF
-	display: block
-	position: absolute
-	text-align: right
-	right: 10px
-	z-index: 1
-	top: 11px
-	background-color: rgba(0, 0, 0, 0.4)
-	background: linear-gradient(135deg, rgba(0,0,0,.4),rgba(0,0,0,.8))
-	padding: 3px 5px
-	border-radius: 4px
-	font-size: 12px
-	max-width: 7em
-	backdrop-filter: blur(8px)
-	white-space: nowrap
-	overflow: hidden
-	text-overflow: ellipsis
-	transition: all .2s
-	will-change: transform
 .poka
-	&.cards .card
-		&[data-source]:hover:before
-			transform: translateY(-5px)
-		.image
+	&.cards
+		--text-color: #000
+		max-width: calc(100% + 8px * 2)
+		width: calc(100% + 8px * 2)
+		margin: -8px
+		display: grid
+		grid-template-columns: repeat(auto-fill,minmax(192px, 1fr))
+		.card
 			height: 0
-			padding-bottom: 100%
+			font-size: 11px
+			margin: .8em 4px
+			background-position: center
 			background-size: cover
-			border-radius: 16px
-			transition: all .2s
-			will-change: box-shadow, transform, filter
+			padding: .475em .5em
+			display: block
+			height: auto
+			padding-bottom: 0
+			text-decoration: none
 			position: relative
-			background-color: var(--card-background-color,#607D8B)
-			.v-image
+			&[data-source]:before
+				content: attr(data-source)
+				position: absolute
+				color: #FFF
+				display: block
+				position: absolute
+				text-align: right
+				right: 10px
+				z-index: 1
+				top: 11px
+				background-color: rgba(0, 0, 0, 0.4)
+				background: linear-gradient(135deg, rgba(0,0,0,.4),rgba(0,0,0,.8))
+				padding: 3px 5px
+				border-radius: 4px
+				font-size: 12px
+				max-width: 7em
+				backdrop-filter: blur(8px)
+				white-space: nowrap
+				overflow: hidden
+				text-overflow: ellipsis
+				transition: all .2s
+				will-change: transform
+			&[data-source]:hover:before
+				transform: translateY(-5px)
+			.image
+				height: 0
+				padding-bottom: 100%
+				background-size: cover
 				border-radius: 16px
-		&:hover .image
-			transform: translateY(-5px)
-			.cover-shadow
-				transform: translateY(13px)
-				opacity: .7
-		&:active .image
-			transform: translateY(-5px)
-			filter: brightness(80%)
-		.title
-			font-size: 1.5em !important
-			padding-top: 5px
-			line-height: 1.5em
-			font-weight: bold
-			color: var(--text-color)
-			opacity: .9
-		.subtitle
-			font-size: 1em !important
-			padding-top: 0px
-			line-height: 1.5em
-			color: var(--text-color)
-			opacity: .75
-		.icon
-			position: absolute
-			background-color: rgba(0,0,0,.75)
-			background: linear-gradient(135deg, rgba(0,0,0,.4),rgba(0,0,0,.8))
-			opacity: 1
-			backdrop-filter: blur(8px)
-			border-radius: 16px
-			padding: 12px
-			box-shadow: 0 4px 6px rgba(0, 0, 0, 0.25)
-			bottom: 0
-			right: 0
-			transform: translate(-3px,-3px)
-			i
-				font-size: 24px
-				color: #fff
-		&:nth-child(14n+2)
-			--card-background-color: #F44336
-		&:nth-child(14n+3)
-			--card-background-color: #E91E63
-		&:nth-child(14n+4)
-			--card-background-color: #9C27B0
-		&:nth-child(14n+5)
-			--card-background-color: #FF9800
-		&:nth-child(14n+6)
-			--card-background-color: #2196F3
-		&:nth-child(14n+7)
-			--card-background-color: #00BCD4
-		&:nth-child(14n+8)
-			--card-background-color: #009688
-		&:nth-child(14n+9)
-			--card-background-color: #4CAF50
-		&:nth-child(14n+10)
-			--card-background-color: #8BC34A
-		&:nth-child(14n+11)
-			--card-background-color: #FFC107
-		&:nth-child(14n+12)
-			--card-background-color: #3F51B5
-		&:nth-child(14n+13)
-			--card-background-color: #FF5722
-		&:nth-child(14n+14)
-			--card-background-color: #795548
-	&.two.cards .card
-		--size: calc(100% / 2 - 8px)
-	&.three.cards .card
-		--size: calc(100% / 3 - 8px)
-	&.four.cards .card
-		--size: calc(100% / 4 - 8px)
-	&.five.cards .card
-		--size: calc(100% / 5 - 8px)
-	&.six.cards .card
-		--size: calc(100% / 6 - 8px)
-	&.sever.cards .card
-		--size: calc(100% / 7 - 8px)
-	&.eight.cards .card
-		--size: calc(100% / 8 - 8px)
-	&.nine.cards .card
-		--size: calc(100% / 9 - 8px)
-	&.ten.cards .card
-		--size: calc(100% / 10 - 8px)
-
-@media (max-width: 380px)
-	.poka
-		&.cards .card
-			--size: calc(100% - 8px)
-		&.doubling.four.cards .card
-			--size: calc(100% - 8px)
-@media (max-width: 575.98px)
-	.poka
-		&.cards .card
-			--size: calc(33.3% - 8px)
-		&.doubling.four.cards .card
-			--size: calc(50% - 8px)
-
-@media (min-width: 576px) and (max-width: 767.98px)
+				transition: all .2s
+				will-change: box-shadow, transform, filter
+				position: relative
+				background-color: var(--card-background-color,#607D8B)
+				.v-image
+					border-radius: 16px
+			&:hover .image
+				transform: translateY(-5px)
+				.cover-shadow
+					transform: translateY(13px)
+					opacity: .7
+			&:active .image
+				transform: translateY(-5px)
+				filter: brightness(80%)
+			.title
+				font-size: 1.5em !important
+				padding-top: 5px
+				line-height: 1.5em
+				font-weight: bold
+				color: var(--text-color)
+				opacity: .9
+			.subtitle
+				font-size: 1em !important
+				padding-top: 0px
+				line-height: 1.5em
+				color: var(--text-color)
+				opacity: .75
+			.icon
+				position: absolute
+				background-color: rgba(0,0,0,.75)
+				background: linear-gradient(135deg, rgba(0,0,0,.4),rgba(0,0,0,.8))
+				opacity: 1
+				backdrop-filter: blur(8px)
+				border-radius: 16px
+				padding: 12px
+				box-shadow: 0 4px 6px rgba(0, 0, 0, 0.25)
+				bottom: 0
+				right: 0
+				transform: translate(-3px,-3px)
+				i
+					font-size: 24px
+					color: #fff
+			&:nth-child(14n+2)
+				--card-background-color: #F44336
+			&:nth-child(14n+3)
+				--card-background-color: #E91E63
+			&:nth-child(14n+4)
+				--card-background-color: #9C27B0
+			&:nth-child(14n+5)
+				--card-background-color: #FF9800
+			&:nth-child(14n+6)
+				--card-background-color: #2196F3
+			&:nth-child(14n+7)
+				--card-background-color: #00BCD4
+			&:nth-child(14n+8)
+				--card-background-color: #009688
+			&:nth-child(14n+9)
+				--card-background-color: #4CAF50
+			&:nth-child(14n+10)
+				--card-background-color: #8BC34A
+			&:nth-child(14n+11)
+				--card-background-color: #FFC107
+			&:nth-child(14n+12)
+				--card-background-color: #3F51B5
+			&:nth-child(14n+13)
+				--card-background-color: #FF5722
+			&:nth-child(14n+14)
+				--card-background-color: #795548
+@media (min-width: 768px)
 	.poka.cards .card
-		--size: calc(33.3% - 8px)
-
-@media (min-width: 768px) and (max-width: 991.98px)
-	.poka.cards .card
-		--size: calc(25% - 8px)
-
-@media (min-width: 992px) and (max-width: 1199.98px)
-	.poka.cards .card
-		--size: calc(20% - 8px)
-
-@media (min-width: 1200px)
-	.poka.cards .card
-		--size: calc(16.6% - 8px)
-
-@media (min-width: 1500px)
-	.poka.cards .card
-		--size: calc(100% / 7 - 8px)
-
-@media (min-width: 1800px)
-	.poka.cards .card
-		--size: calc(100% / 8 - 8px)
-
-@media (max-width: 575.98px)
-	.poka
-		&.doubling.ten.cards .card
-			--size: calc(20% - 8px)
-		&.color.picker.cards > .card
-			--size: calc(20%)
+		grid-template-columns: repeat(auto-fill,minmax(128px, 1fr))
 </style>
