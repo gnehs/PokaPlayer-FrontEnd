f832213d31bd5bad53db1248538043500415d2e1
gnehs <jayuiop@gmail.com>
2021-07-23 22:34:54 +0800

    fixed router-view padding

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -106,9 +106,7 @@ nav
       border-radius: 0
 .router-view
   min-height: calc(100% - 69px)
-@media (min-width: 576px)
-  .router-view
-    padding: 16px
+  padding: 16px
 
 .v-app-bar
   box-shadow: inset 0 -1px 0 0 #dadce0
