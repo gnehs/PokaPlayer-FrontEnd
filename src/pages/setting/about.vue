dfd7a69145dfc241942bf86345bb7dc73691854a
gnehs <jayuiop@gmail.com>
2021-08-03 13:31:43 +0800

    updated about to pokalist

--- src/pages/setting/about.vue
+++ src/pages/setting/about.vue
@@ -1,41 +1,43 @@
 <template>
   <div>
-    <v-list subheader>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-icon class="bx">bx-info-circle</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>PokaPlayer</v-list-item-title>
-          <v-list-item-subtitle>{{ poka_version }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+    <div class="poka list">
+      <div class="item" v-ripple>
+        <div class="content">
+          <v-icon class="bx mx-2">bx-info-circle</v-icon>
+          <div class="header">
+            <div class="head t-ellipsis">PokaPlayer</div>
+            <div class="t-ellipsis">
+              {{ poka_version }}
+            </div>
+          </div>
+        </div>
+      </div>
       <v-divider />
-      <v-list-item href="https://github.com/gnehs/PokaPlayer/" target="_blank">
-        <v-list-item-avatar>
-          <v-icon class="bx">bxl-github</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>GitHub</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item href="https://github.com/gnehs/PokaPlayer/graphs/contributors" target="_blank">
-        <v-list-item-avatar>
-          <v-icon class="bx">bx-user</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ $t('settings_about_contributions') }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item href="https://github.com/gnehs/PokaPlayer/issues" target="_blank">
-        <v-list-item-avatar>
-          <v-icon class="bx">bx-error</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ $t('settings_about_errorEeport') }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+      <a class="item" v-ripple href="https://github.com/gnehs/PokaPlayer/" target="_blank">
+        <div class="content">
+          <v-icon class="bx mx-2">bxl-github</v-icon>
+          <div class="header">
+            <div class="head t-ellipsis">GitHub</div>
+          </div>
+        </div>
+      </a>
+      <a class="item" v-ripple href="https://github.com/gnehs/PokaPlayer/graphs/contributors" target="_blank">
+        <div class="content">
+          <v-icon class="bx mx-2">bx-user</v-icon>
+          <div class="header">
+            <div class="head t-ellipsis">{{ $t('settings_about_contributions') }}</div>
+          </div>
+        </div>
+      </a>
+      <a class="item" v-ripple href="https://github.com/gnehs/PokaPlayer/issues" target="_blank">
+        <div class="content">
+          <v-icon class="bx mx-2">bx-error</v-icon>
+          <div class="header">
+            <div class="head t-ellipsis">{{ $t('settings_about_errorEeport') }}</div>
+          </div>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
