2a6c0466b13896ae88bdf2e9452229f9e73b2225
gnehs <jayuiop@gmail.com>
2021-12-30 22:00:50 +0800

    updated review page

--- src/pages/Review.vue
+++ src/pages/Review.vue
@@ -8,15 +8,15 @@
     <poka-loader v-if="!data" />
     <div v-if="data">
       <h1 class="text-center text-h3 font-weight-thin review-title">嘿</h1>
-      <p class="text-center">去年你一共在 PokaPlayer 聽了 {{ data.total }} 首歌，在 {{ data.days[0]._id }} 聽了最多首歌，居然有 {{ data.days[0].count }} 首！</p>
+      <p
+        class="text-center"
+      >去年你一共在 PokaPlayer 聽了 {{ data.total }} 首歌，在 {{ data.days[0]._id }} 聽了最多首歌，居然有 {{ data.days[0].count }} 首！</p>
       <h1 class="text-center text-h3 font-weight-thin review-title">收聽最多的歌曲</h1>
       <poka-parse-songs :data="data.songs" />
       <h1 class="text-center text-h3 font-weight-thin review-title">收聽最多的專輯</h1>
       <poka-parse-albums :data="data.albums" />
       <h1 class="text-center text-h3 font-weight-thin review-title">收聽最多的演出者</h1>
       <poka-parse-artists :data="data.artists" />
-      <h1 class="text-center text-h3 font-weight-thin review-title">版本更新</h1>
-      <h1 class="text-center text-h3 font-weight-thin review-title">社群貢獻</h1>
       <h1 class="text-center text-h3 font-weight-thin review-title">感謝有你</h1>
       <p class="text-center text-h5 font-weight-thin">感謝你使用 PokaPlayer，希望未來可以陪伴你走過更多時光！</p>
     </div>
