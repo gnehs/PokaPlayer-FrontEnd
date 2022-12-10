912d400ffe63e74e72e6c6d7886a783fdd37eb64
gnehs <jayuiop@gmail.com>
2022-12-10 16:41:11 +0800

    moved p-btn, p-input to folder

--- src/components/P/Btn.vue
+++ src/components/P/Btn.vue
@@ -8,7 +8,7 @@ export default {
   props: {
     color: {
       type: String,
-      default: 'primary'
+      default: ''
     },
     outline: {
       type: Boolean,
@@ -33,17 +33,25 @@ export default {
 .p-btn
   color: var(--text-color)
   background-color: var(--background-layer-2)
-  padding: var(--padding)
+  padding: var(--padding) calc(var(--padding) * 2)
   border: none
   border-radius: var(--border-radius)
   cursor: pointer
   transition: all var(--transition)
   font-size: 14px
   line-height: 1.5
+  font-weight: bold
+  display: flex
+  align-items: center
+  justify-content: center
+  gap: var(--padding)
   &:hover
     background-color: var(--background-layer-3)
   &:active
     background-color: var(--background-layer-4)
+  &[color="primary"]
+    background-color: var(--primary-color)
+    color: #FFF
   &.outline
     background-color: transparent
     border: 1px solid var(--background-layer-2)
--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const components = import.meta.globEager('./components/**/*.vue')
 Object.entries(components).forEach(([path, definition]) => {
   // Get name of component, based on filename
   // "./components/Fruits.vue" will become "Fruits"
-  const componentName = path.split('/').pop().replace(/\.\w+$/, '')
+  const componentName = path.split('/').slice(2).join('').replace(/\.\w+$/, '')
 
   // Register component on this Vue instance
   app.component(componentName, definition.default)
