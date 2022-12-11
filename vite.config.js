26202f4d1aa5caec2eef860971124276466ae606
gnehs <jayuiop@gmail.com>
2022-12-11 20:20:17 +0800

    added PWA support

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,27 @@ export default defineConfig({
     vue(),
     vueI18n({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**')
+    }),
+    VitePWA({
+      includeAssets: ['favicon.ico', '/img/apple-touch-icon.png', '/img/icon.svg'],
+      manifest: {
+        name: 'PokaPlayer',
+        short_name: 'PokaPlayer',
+        description: 'PokaPlayer is a player that can unify and play from multiple sources like DSM and Netease.',
+        theme_color: '#f2f2f2',
+        icons: [
+          {
+            src: '/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/img/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
   resolve: {
