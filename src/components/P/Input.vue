0a3d7797e6b099191b18da74889d0849407cd0d5
gnehs <jayuiop@gmail.com>
2022-12-12 01:24:55 +0800

    fixed input class name

--- src/components/P/Input.vue
+++ src/components/P/Input.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="poka-text-input" :class="{   value: value.length   }">
+  <div class="poka-text-input" :class="{ value: value.length }">
     <label class="poka-text-input__label" :for="randomInputId">
       <span class="poka-text-input__label-text">{{ label }}</span>
     </label>
@@ -55,15 +55,15 @@ export default {
       color: var(--text-color)
       transition: all var(--transition)
   &:not(.value):not(:has(input:focus))
-    .poka-text-input-label
+    .poka-text-input__label
       cursor: text
-      .poka-text-input-label-text
+      .poka-text-input__label-text
         transform: translateY(calc(var(--padding) * 2 + 16px)) translateX(var(--padding)) scale(1.1429)
         opacity: .5
 
   &:has(input:focus)
-    .poka-text-input-label
-      .poka-text-input-label-text
+    .poka-text-input__label
+      .poka-text-input__label-text
         color: var(--primary-color)
         font-weight: bold
   .poka-text-input__input
