65029c5bb43dab1f42b6e832a6116d2a9419459d
gnehs <jayuiop@gmail.com>
2023-01-02 14:42:59 +0800

    updated log page

--- src/pages/settings/Log.vue
+++ src/pages/settings/Log.vue
@@ -1,34 +1,49 @@
 <script setup>
-import { ref, computed, watch, inject, onMounted } from 'vue'
+import { ref, inject, onMounted } from 'vue'
 import { useIntersectionObserver } from '@vueuse/core'
 const PokaAPI = inject('PokaAPI')
-const logs = ref([])
-const loading = ref(true)
+const logs = ref('')
 const page = ref(0)
 const end = ref(false)
+const users = ref([])
+async function getUserList() {
+  let data = await PokaAPI.getUserList()
+  users.value = data
+}
+async function getLogs(p = 0) {
+  if (!users.value || !users.value.length) {
+    await getUserList()
+  }
+  let res = await PokaAPI.getLog(p)
+  for (let { level, type, event: _event, user, description, time } of res) {
+    user = users.value.find(u => u._id == user)?.username || user
+    time = new Date(time).toLocaleString()
+    users.value.map(u => {
+      description = description.replace(new RegExp(`{${u._id}}`, 'g'), u.username)
+    })
 
-async function getLogs() {
-  loading.value = true
-  let res = await PokaAPI.getLog(page.value)
-  logs.value = logs.value.concat(res)
+    logs.value += `[${level}] ${type} / ${_event}\n`
+    logs.value += `  📄 ${description}\n`
+    logs.value += `  👤 ${user}\n`
+    logs.value += `  🕒 ${time}\n`
+    logs.value += `\n`
+  }
   if (!res.length) {
     end.value = true
   }
-  loading.value = false
 }
-
 // auto infinite scroll
 const logBottom = ref(null)
 const logBottomVisible = useIntersectionObserver(logBottom, async ([{ isIntersecting }], observerElement) => {
   if (isIntersecting) {
-    page.value++
-    await getLogs()
+    await getLogs(page.value++)
   }
 }, {
   threshold: 0.5,
   rootMargin: '0px',
 })
 onMounted(async () => {
+  await getUserList()
   await getLogs()
 })
 
@@ -37,6 +52,13 @@ onMounted(async () => {
   <Teleport to="#header-center">
     <p>{{ $t('settings.log.title') }}</p>
   </Teleport>
-  <pre ref="logContainer">{{ logs }}</pre>
+  <pre class="log" ref="logContainer">{{ logs }}</pre>
   <Loader ref="logBottom" v-if="!end" />
 </template>
+<style lang="sass" scoped>
+pre.log
+  overflow-x: auto
+  padding: var(--padding)
+  white-space: pre-wrap
+  word-break: break-all
+</style>
