ebfc931c68c0734bf5b2e2413db98da3fe31c894
gnehs <jayuiop@gmail.com>
2023-01-17 14:59:03 +0800

    lazy loading list imgs

--- src/components/P/List/ItemImg.vue
+++ src/components/P/List/ItemImg.vue
@@ -1,12 +1,24 @@
-
 <script setup>
+import { ref } from 'vue'
+import { useIntersectionObserver } from '@vueuse/core'
 const props = defineProps({
   src: String,
   alt: String,
 })
+const imgContainer = ref(null)
+const imgContainerIsVisible = ref(false)
+const { stop } = useIntersectionObserver(
+  imgContainer,
+  ([{ isIntersecting }], observerElement) => {
+    imgContainerIsVisible.value = imgContainerIsVisible.value || isIntersecting
+  },
+)
 </script>
 <template>
-  <div class="p-list-item__img">
-    <img :src="src" :alt="alt" loading="lazy" />
+  <div class="p-list-item__img" ref="imgContainer">
+    <transition name="fade" mode="out-in">
+      <img v-if="imgContainerIsVisible" :src="src" :alt="alt" />
+      <img v-else />
+    </transition>
   </div>
 </template>
